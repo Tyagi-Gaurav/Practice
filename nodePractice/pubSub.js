a2afe5206d825ebf3e2b317ca7d1da13063ae6ad
gtyagi <v-gtyagi@ean.com>
2016-11-25 17:24:18 +0000

    More Node Practice

--- nodePractice/pubSub.js
+++ nodePractice/pubSub.js
@@ -3,31 +3,40 @@ var net = require('net');
 var events = require('events')
 var channel = new events.EventEmitter();
 
-channel.clients = [];
+channel.clientSockets = [];
 channel.subscriptions = {};
 
 channel.on('join', function (id, client) {
     console.log('Client joined on channel');
-    this.clients[id] = client;
+    this.clientSockets[id] = client;
     this.subscriptions[id] = function(senderId, message) {
         console.log('Broadcasting Data from sender: ' + senderId + ', ' + message);
         if (id != senderId) {
-            this.clients[id].write(message);
+            this.clientSockets[id].write(message);
         }
     };
 
     this.on('broadcast', this.subscriptions[id]);
 });
 
-var server = net.createServer(function(client) {
-    var id = client.remoteAddress + ':' + client.remotePort;
-    console.log('Client id ' + id);
-    channel.emit('join', id, client);
+channel.on('leave', function(id) {
+   channel.removeListener('broadcast', this.subscriptions[id]);
+    channel.emit('broadcast', id, id + ' has left the chat.');
+});
 
-    client.on('data', function(data) {
+var server = net.createServer(function(socket) {
+    var id = socket.remoteAddress + ':' + socket.remotePort;
+    console.log('Client id ' + id);
+    channel.emit('join', id, socket);
+
+    socket.on('data', function(data) {
         data = data.toString();
         console.log('Client Data Broadcasting: ' + data);
        channel.emit('broadcast', id, data);
+    });
+
+    socket.on('leave', function(id) {
+       channel.emit('leave', id);
     });
 });
 
