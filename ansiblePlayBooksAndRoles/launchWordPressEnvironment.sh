b93fff822b4cb28806774f2b335fe57dd77566b1
gtyagi <v-gtyagi@ean.com>
2017-01-09 16:55:29 +0000

    Modified user removal process for OS hardening and creating RDS environment

--- ansiblePlayBooksAndRoles/launchWordPressEnvironment.sh
+++ ansiblePlayBooksAndRoles/launchWordPressEnvironment.sh
@@ -8,10 +8,10 @@ USER_APP_TIER_TAG="UserApplication"
 EXTRA_VARS="--extra-vars class_tag=integration_test_instance --extra-vars db_tier_tag=$USER_DB_TIER_TAG --extra-vars app_tier_tag=$USER_APP_TIER_TAG"
 
 #Create VM
-ansible-playbook -i localhost playbooks/createCloudInstances.yml $EXTRA_VARS
+ansible-playbook -i localhost playbooks/create_rds_instances.yml $EXTRA_VARS
 
-export ANSIBLE_HOST=../../ec2.py
-export EC2_INI_PATH=../../ec2.ini
+export ANSIBLE_HOST=./ec2.py
+export EC2_INI_PATH=./ec2.ini
 
 #Refresh Ansible Cache so it has latest ec2 inventory information.
 $ANSIBLE_HOST --refresh-cache > /dev/null
