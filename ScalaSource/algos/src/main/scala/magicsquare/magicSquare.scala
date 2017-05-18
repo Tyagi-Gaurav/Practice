dd2bc6bd5b8126dc79dbaca99dea717d2e0a88cf
gtyagi <v-gtyagi@ean.com>
2017-05-18 09:23:41 +0100

    Magic Square Test

--- ScalaSource/algos/src/main/scala/magicsquare/magicSquare.scala
+++ ScalaSource/algos/src/main/scala/magicsquare/magicSquare.scala
@@ -2,23 +2,40 @@ package magicsquare
 
 import scala.io.StdIn
 
-class magicSquare {
-  def main(args: Array[String]) {
+object magicSquare extends App {
 
-    val ip1_rows = StdIn.readInt
-    val ip1_cols = StdIn.readInt
-    val ip1 = Array.ofDim[Int](ip1_rows, ip1_cols)
+//    val ip1_rows = StdIn.readInt
+//    val ip1_cols = StdIn.readInt
+//    val ip1 = Array.ofDim[Int](ip1_rows, ip1_cols)
+//    for(ip1_i <- 0 until ip1_rows)
+//      ip1(ip1_i) = scala.io.StdIn.readLine().split(" ").map(_.toInt)
+//    val output = SolveMagicSquare(ip1);
+//    println(output)
 
-    for(ip1_i <- 0 until ip1_rows)
-      ip1(ip1_i) = scala.io.StdIn.readLine().split(" ").map(_.toInt)
+    val potMagcSquare = Array(
+          Array(1, 2, 3),
+          Array(3, 1, 2),
+          Array(2, 3, 1))
 
+    val potMagcSquare1 = Array(
+      Array(1, 2, 3),
+      Array(3, 1, 2),
+      Array(2, 4, 1))
 
-    val output = SolveMagicSquare(ip1);
-    println(output)
-    println ("Total Memory Used" + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()))
-  }
+    println (isMagicSquare(potMagcSquare))
+    println (isMagicSquare(potMagcSquare1))
+
+  def setBitForNumber(num : Int, bitNum : Int) = num | (0x01 << bitNum - 1)
 
   def SolveMagicSquare(input1: Array[Array[Int]]): Int = {
     0
+  }
+
+  def isMagicSquare(input1: Array[Array[Int]]) : Boolean = {
+    val outputSet = input1.map(x =>
+      x.foldLeft(0x0)((a, b) => setBitForNumber(a, b))).toSet
+
+    outputSet.size == 1 &&
+      outputSet.contains(math.pow(2, input1.size).toInt - 1)
   }
 }
---
+++ ScalaSource/algos/src/test/scala/magicsquare/MagicSquareTest.scala
@@ -0,0 +1,9 @@
+package magicsquare
+
+import org.scalatest.WordSpec
+
+class MagicSquareTest extends WordSpec {
+  "Should be able to verify magic square" in {
+
+  }
+}
