29a3c766196ab058eb733d7cb779d0c053f9d4bf
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2018-12-27 01:11:53 +0530

    Kafka Safe Producer

--- Kafka/src/main/java/org/gt/simplekafka/twitter/TwitterProducer.java
+++ Kafka/src/main/java/org/gt/simplekafka/twitter/TwitterProducer.java
@@ -90,6 +90,12 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class.getName());
 
+        //Create Safe Producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, String.valueOf(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         //Create Producer
         return new KafkaProducer<>(properties);
     }
