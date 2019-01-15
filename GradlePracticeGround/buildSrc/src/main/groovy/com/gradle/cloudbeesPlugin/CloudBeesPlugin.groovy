758935856afe913685550b757db9e7b23d6f3160
Gaurav Tyagi <aanconsultancyservicesltd@gmail.com>
2019-01-15 05:37:42 +0000

    Plugin extensions and convention mapping

--- GradlePracticeGround/buildSrc/src/main/groovy/com/gradle/cloudbeesPlugin/CloudBeesPlugin.groovy
+++ GradlePracticeGround/buildSrc/src/main/groovy/com/gradle/cloudbeesPlugin/CloudBeesPlugin.groovy
@@ -6,19 +6,24 @@ import org.gradle.api.Project
 import org.gradle.api.plugins.WarPlugin
 
 class CloudBeesPlugin implements Plugin<Project> {
+    static final String EXTENSION_NAME = "cloudbees"
 
     @Override
     void apply(Project project) {
         project.plugins.apply(WarPlugin)
+        //Register the extension container with the name cloudbees
+        project.extensions.create(EXTENSION_NAME, CloudBeesPluginExtension)
         addTasks(project)
     }
 
     private void addTasks(Project project) {
         project.tasks.withType(AppInfo) {
             //For all tasks of type AppInfo, provide following properties.
-            apiUrl = 'https://api.cloudbees.com/api'
-            apiKey = project.property('apikey')
-            apiSecret = project.property('apiSecret')
+            def extension = project.extensions.findByName(EXTENSION_NAME)
+            //Lookup extension values from extension container
+            conventionMapping.apiUrl = {extension.apiUrl}
+            conventionMapping.apiKey = {extension.apiKey}
+            conventionMapping.apiSecret = {extension.apiSecret}
         }
 
         addAppTasks(project)
---
+++ GradlePracticeGround/buildSrc/src/main/groovy/com/gradle/cloudbeesPlugin/CloudBeesPluginExtension.groovy
@@ -0,0 +1,8 @@
+package com.gradle.cloudbeesPlugin
+
+class CloudBeesPluginExtension {
+    String apiUrl
+    String apiKey
+    String apiSecret
+    String appId
+}
